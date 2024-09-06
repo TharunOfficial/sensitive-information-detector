66a7ccac4211867708de38e3962166e4f41ed54f
Tharun M <138559432+TharunOfficial@users.noreply.github.com>
2024-09-06 12:15:26 +0530

    Add files via upload

--- website/static/script.js
+++ website/static/script.js
@@ -1,10 +1,21 @@
+const loadingScreen = document.querySelector('.container');
+const resultDiv = document.getElementById('result');
+const sensitiveDataList = document.getElementById('sensitive-data-list');
+const blurredImageContainer = document.getElementById('blurred-image-container');
+
 document.getElementById('upload-form').addEventListener('submit', async (e) => {
     e.preventDefault();
-    
+
+   
+    loadingScreen.style.display = 'block';
+    loadingScreen.classList.add('loading');
+    resultDiv.style.display = 'none'; 
+
     const formData = new FormData();
     formData.append('file', document.getElementById('file-input').files[0]);
 
     try {
+       
         const response = await fetch('/upload', {
             method: 'POST',
             body: formData
@@ -13,16 +24,12 @@ document.getElementById('upload-form').addEventListener('submit', async (e) => {
         if (!response.ok) throw new Error('Network response was not ok.');
 
         const result = await response.json();
-        
-        // Clear previous results
-        const resultDiv = document.getElementById('result');
-        const sensitiveDataList = document.getElementById('sensitive-data-list');
-        const blurredImageContainer = document.getElementById('blurred-image-container');
-        
+
+      
         sensitiveDataList.innerHTML = '';
         blurredImageContainer.innerHTML = '';
 
-        // Display sensitive data
+      
         if (result.sensitive_data.length > 0) {
             result.sensitive_data.forEach(([label, value]) => {
                 const listItem = document.createElement('li');
@@ -33,7 +40,7 @@ document.getElementById('upload-form').addEventListener('submit', async (e) => {
             sensitiveDataList.innerHTML = '<li>No sensitive data detected.</li>';
         }
 
-        // Display blurred images
+    
         if (result.blurred_image_urls && result.blurred_image_urls.length > 0) {
             result.blurred_image_urls.forEach((url) => {
                 const link = document.createElement('a');
@@ -47,10 +54,19 @@ document.getElementById('upload-form').addEventListener('submit', async (e) => {
             blurredImageContainer.innerHTML = 'No blurred images available.';
         }
 
-        resultDiv.style.display = 'block';
+        
+        setTimeout(() => {
+            resultDiv.style.display = 'block';
+            loadingScreen.style.display = 'none'; 
+        }, 1800); 
     } catch (error) {
         console.error('Error:', error);
         alert('There was an error uploading the file.');
+
+        
+        setTimeout(() => {
+            loadingScreen.classList.remove('loading');
+            loadingScreen.style.display = 'none';
+        }, 1800); 
     }
 });
-
